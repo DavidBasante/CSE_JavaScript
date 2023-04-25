d5eb46751b2d8bac916dac144f0b94985c95a38d
David Basante <dsbasante@gmail.com>
2023-04-25 17:41:42 -0300

    cod complete

--- Loop/Program.cs
+++ Loop/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rand = new Random();
+int magicNum = rand.Next(0, 100);
+
+int guess = 0;
+
+
+while (guess != magicNum){
+    Console.Write("What is the magic number? ");
+    guess = int.Parse(Console.ReadLine());
+
+    if (magicNum > guess){
+        Console.WriteLine("Higher");
+    }
+    else if (magicNum < guess){
+        Console.WriteLine("Lower");
+    }
+    else{
+        Console.WriteLine("You rock!!!");
+    }
+
+}
+Console.ReadKey();
