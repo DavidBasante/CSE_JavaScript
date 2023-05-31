8996fab2528bb83405c8a0f83405aa6a8f39458b
David Basante <dsbasante@gmail.com>
2023-05-31 13:25:28 -0300

    commit

--- week05/student_files/scripts/task5.js
+++ week05/student_files/scripts/task5.js
@@ -3,37 +3,30 @@
 /* IF/ELSE IF */
 
 // Step 1: Declare and initialize a new variable to hold the current date
-
 const currentDate = new Date();
 
 // Step 2: Declare another variable to hold the day of the week
-
-const weekDay = currentDate.getDate();
+const weekDay = currentDate.getDay();
 
 // Step 3: Using the variable declared in Step 1, assign the value of the variable declared in Step 2 to the day of the week ( hint: getDay() )
 // Step 4: Declare a variable to hold a message that will be displayed
-
 let message;
 
 // Step 5: Using an if statement, if the day of the week is a weekday (i.e. Monday - Friday), set the message variable to the string 'Hang in there!'
-// Step 6: Using an else statement, set the message variable to 'Woohoo!  It is the weekend!'
-
-if (weekDay >= 1 && weekDay <= 5){
+// Step 6: Using an else statement, set the message variable to 'Woohoo! It is the weekend!'
+if (weekDay >= 1 && weekDay <= 5) {
     message = "Hang in there!";
-}
-else{
-    message = "Woohoo! It is the weekend!"
+} else {
+    message = "Woohoo! It is the weekend!";
 }
 
 /* SWITCH, CASE, BREAK */
 
 // Step 1: Declare a new variable to hold another message
-
 let message2;
 
 // Step 2: Use switch, case and break to set the message variable to the day of the week as a string (e.g. Sunday, Monday, etc.) using the day of week variable declared in Step 2 above
-
-switch (weekDay){
+switch (weekDay) {
     case 0:
         message2 = 'Sunday';
         break;
@@ -43,7 +36,7 @@ switch (weekDay){
     case 2:
         message2 = "Tuesday";
         break;
-        case 3:
+    case 3:
         message2 = "Wednesday";
         break;
     case 4:
@@ -54,21 +47,19 @@ switch (weekDay){
         break;
     case 6:
         message2 = "Saturday";
+        break;
 }
 
 /* OUTPUT */
 
 // Step 1: Assign the value of the first message variable to the HTML element with an ID of message1
-
 document.getElementById('message1').textContent = message;
 
 // Step 2: Assign the value of the second message variable to the HTML element with an ID of message2
-
 document.getElementById('message2').textContent = message2;
 
 /* FETCH */
 // Step 1: Declare a global empty array variable to store a list of temples
-
 let templeList = [];
 
 // Step 2: Declare a function named output that accepts a list of temples as an array argument and does the following for each temple:
@@ -79,7 +70,6 @@ let templeList = [];
 // - Creates an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's templeName property to the alt attribute
 // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
 // - Appends the <article> element to the HTML element with an ID of temples
-
 function output(temples) {
     let divContainer = document.getElementById("temples");
 
@@ -112,8 +102,6 @@ function output(temples) {
 // Step 4: In the function, using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'. Create a variable to hold the response from your fetch. You should have the program wait on this line until it finishes.
 // Step 5: Convert your fetch response into a Javascript object ( hint: .json() ). Store this in the templeList variable you declared earlier (Step 1). Make sure the the execution of the code waits here as well until it finishes.
 // Step 6: Finally, call the output function and pass it the list of temples. Execute your getTemples function to make sure it works correctly.
-
-
 async function getTemples() {
     const response = await fetch("https://byui-cse.github.io/cse121b-course/week05/temples.json");
     const data = await response.json();
@@ -123,7 +111,6 @@ async function getTemples() {
 getTemples();
 
 // Step 7: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
-
 function reset() {
     const divContainer = document.getElementById('temples');
     while (divContainer.firstChild) {
@@ -135,7 +122,6 @@ function reset() {
 // - Calls the reset function
 // - Sorts the global temple list by the currently selected value of the HTML element with an ID of sortBy
 // - Calls the output function passing in the sorted list of temples
-
 function sortBy() {
     reset();
     const sortByValue = document.getElementById('sortBy').value;
@@ -144,9 +130,6 @@ function sortBy() {
     });
     output(templeList);
 }
+
 // Step 9: Add a change event listener to the HTML element with an ID of sortBy that calls the sortBy function
 document.getElementById('sortBy').addEventListener('change', sortBy);
-/* STRETCH */
-
-// Consider adding a "Filter by" feature that allows users to filter the list of temples
-// This will require changes to both the HTML and the JavaScript files
